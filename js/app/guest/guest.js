bc4189ab37be501e0bbb2040a5dc4caf459a22ac
Fadlhi Anggara Senoaji <anggaraffadlhi@gmail.com>
2025-06-18 07:38:13 +0700

    First Commit

--- js/app/guest/guest.js
+++ js/app/guest/guest.js
@@ -251,10 +251,10 @@ export const guest = (() => {
         const url = new URL('https://calendar.google.com/calendar/render');
         const data = new URLSearchParams({
             action: 'TEMPLATE',
-            text: 'The Wedding of Wahyu and Riski',
-            dates: `${formatDate('2023-03-15 10:00')}/${formatDate('2023-03-15 11:00')}`,
+            text: 'The Wedding of Fadlhi and Aliyah',
+            dates: `${formatDate('2025-06-29 09:00')}/${formatDate('2025-06-29 12:00')}`,
             details: 'Tanpa mengurangi rasa hormat, kami mengundang Anda untuk berkenan menghadiri acara pernikahan kami. Terima kasih atas perhatian dan doa restu Anda, yang menjadi kebahagiaan serta kehormatan besar bagi kami.',
-            location: 'RT 10 RW 02, Desa Pajerukan, Kec. Kalibagor, Kab. Banyumas, Jawa Tengah 53191.',
+            location: 'Jalan GA. Manulang RT 03 RW 03, Desa Jayamekar, Padalarang, Kab. Bandung Barat, Jawa Barat.',
             ctz: config.get('tz'),
         });
 
